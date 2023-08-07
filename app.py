70827932b9b13dd60a642e8cdf627ad1b3f3c608
Filip Wojda <wojdafilipdev@gmail.com>
2023-08-07 14:30:36 -0400

    final working app

--- app.py
+++ app.py
@@ -2,6 +2,11 @@ from flask import Flask, render_template, redirect, url_for, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, UserMixin, login_user, logout_user
 import requests
+import os
+from dotenv import load_dotenv
+import random
+
+load_dotenv()
 
 app = Flask(__name__)
 
@@ -23,6 +28,11 @@ class Users(UserMixin, db.Model):
                          nullable=False)
     password = db.Column(db.String(250),
                          nullable=False)
+    
+class TrackRecommendations(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    url = db.Column(db.String(500), unique=True, nullable=False)
+
  
  
 # Initialize app with extension
@@ -71,9 +81,64 @@ def signin():
 
 @app.route("/pick", methods=["GET", "POST"])
 def pick():
+    recommendations = TrackRecommendations.query.all()
+    rec_urls = [rec.url for rec in recommendations]
+    
     if request.method == "POST":
-        pass
-    return render_template("pick.html")
+        db.session.query(TrackRecommendations).delete()
+        db.session.commit()
+        artist_ids = ['6M2wZ9GZgrQXHCFfjv46we', '5pKCCKE2ajJHZ9KAiaK11H', '0hCNtLu0JehylgoiP8L4Gh', '06HL4z0CvFAxyc27GXpf02', '3LZZPxNDGDFVSIPqf4JuEf']
+        artist_choice = random.choice(artist_ids)
+        
+        url = "https://accounts.spotify.com/api/token"
+        headers = {
+            "Content-Type": "application/x-www-form-urlencoded"
+        }
+        data = {
+            "grant_type": "client_credentials",    
+            "client_id": os.getenv("CLIENT_ID"),
+            "client_secret": os.getenv("CLIENT_SECRET")
+        }
+        response = requests.post(url, headers=headers, data=data)
+        res = response.json()
+        access_token = res['access_token']
+        print(access_token)
+        genre1 = request.form.get("genre1")
+        genre2 = request.form.get("genre2")
+        genre3 = request.form.get("genre3")
+            
+        print(genre1, genre2, genre3)
+        
+        rec_url = "https://api.spotify.com/v1/recommendations/"
+        rec_params = {
+            "seed_artists": artist_choice,
+            "seed_genres": f"{genre1},{genre2},{genre3}",
+            "seed_tracks": "0c6xIDDpzE81m2q797ordA"
+        }
+        rec_headers = {
+            "Authorization": f"Bearer {access_token}"
+        }
+
+        rec_res = requests.get(rec_url, headers=rec_headers, params=rec_params).json()
+        
+        spotify_urls = []
+        for track in rec_res['tracks']:
+            spotify_url = track.get('external_urls', {}).get('spotify')
+            if spotify_url and "/track" in spotify_url:
+                spotify_urls.append(spotify_url)
+                
+        for url in spotify_urls:
+            existing_track = TrackRecommendations.query.filter_by(url=url).first()
+            if not existing_track:
+                new_track = TrackRecommendations(url=url)
+                db.session.add(new_track)
+                db.session.commit()
+        print(spotify_urls)
+        
+    if not rec_urls:
+        return render_template("pick.html", message="No recommendations yet")
+    else:
+        return render_template("pick.html", recommendations=rec_urls)
 
 @app.route("/logout")
 def logout():
@@ -86,6 +151,6 @@ def index():
 	return render_template("index.html")
 
 if __name__ == '__main__':
-	app.run(debug=True)
+	app.run(debug=True, host="0.0.0.0")
  
  
