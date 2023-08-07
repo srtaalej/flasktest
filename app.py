cfd0b3001e8902a4c56d22af731fb6ffbf781e56
Filip Wojda <wojdafilipdev@gmail.com>
2023-08-07 00:56:14 -0400

    almost done

--- app.py
+++ app.py
@@ -1,6 +1,94 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, request
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, UserMixin, login_user, logout_user
+import requests
 
 app = Flask(__name__)
+
+# Tells flask-sqlalchemy what database to connect to
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite"
+# Enter a secret key
+app.config["SECRET_KEY"] = "ENTER YOUR SECRET KEY"
+# Initialize flask-sqlalchemy extension
+db = SQLAlchemy()
+ 
+# LoginManager is needed for our application
+# to be able to log in and out users
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+class Users(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(250), unique=True,
+                         nullable=False)
+    password = db.Column(db.String(250),
+                         nullable=False)
+ 
+ 
+# Initialize app with extension
+db.init_app(app)
+# Create database within app context
+ 
+with app.app_context():
+    db.create_all()
+    
+@login_manager.user_loader
+def loader_user(user_id):
+    return Users.query.get(user_id)
+
+@app.route('/register', methods=["GET", "POST"])
+def register():
+  # If the user made a POST request, create a new user
+    if request.method == "POST":
+        user = Users(username=request.form.get("username"),
+                     password=request.form.get("password"))
+        # Add the user to the database
+        db.session.add(user)
+        # Commit the changes made
+        db.session.commit()
+        # Once user account created, redirect them
+        # to login route (created later on)
+        return redirect(url_for("signin"))
+    # Renders sign_up template if user made a GET request
+    return render_template("register.html")
+
+@app.route("/signin", methods=["GET", "POST"])
+def signin():
+    # If a post request was made, find the user by
+    # filtering for the username
+    if request.method == "POST":
+        user = Users.query.filter_by(
+            username=request.form.get("username")).first()
+        # Check if the password entered is the
+        # same as the user's password
+        if user.password == request.form.get("password"):
+            # Use the login_user method to log in the user
+            login_user(user)
+            return redirect(url_for("index"))
+        # Redirect the user back to the home
+        # (we'll create the home route in a moment)
+    return render_template("signin.html")
+
+@app.route("/pick", methods=["GET", "POST"])
+def pick():
+    if request.method == "POST":
+        url = "https://accounts.spotify.com/api/token"
+        headers = {
+            "Content-Type": "application/x-www-form-urlencoded"
+        }
+        data = {
+            "grant_type": "client_credentials",
+            "client_id": "6c169079efe6468daf0c6d2eadc5c063",
+            "client_secret": "b7db0e7aaa6c4143ba35d4b75b886a14"
+        }
+        response = requests.post(url, headers=headers, data=data)
+        print(response.json())
+    return render_template("pick.html")
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for("index"))
 
 @app.route("/")
 @app.route("/index")
@@ -9,3 +97,5 @@ def index():
 
 if __name__ == '__main__':
 	app.run(debug=True)
+ 
+ 
